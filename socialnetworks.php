f7f6345bbf3713a86e31dc383791b5fcf5f217e1
unisoftdev <juraj.vysvader@gmail.com>
2016-03-13 16:31:40 +0000

    Added files via upload

---
+++ socialnetworks.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* @Social networks
+* Created By Juraj Vysvader
+* https://www.unisoftdev.tech/
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @Copyright (C) 2016 Juraj Vysvader. All rights reserved.
+*/
+
+defined('_JEXEC') or die;
+
+class plgContentSocialnetworks extends JPlugin{
+
+	public function onContentAfterDisplay($context, &$article, &$params, $limitstart)
+        {
+                return "<div id='socialshare'></div>";
+        }	
+
+
+	public function onContentPrepare($context, &$row, &$params, $page = 0)
+		{
+				$document = JFactory::getDocument();
+				$document->addStyleSheet(JURI::base(). "plugins/content/socialnetworks/socialnetworks.css");
+				$document->addScript(JURI::base(). "plugins/content/socialnetworks/socialnetworks.js");
+				
+		}		
+}
+
+?>
